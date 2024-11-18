4d316e226f587d57c48393bf747b79e52da0cdaa
Giovanne Britto <gio.nbritto@gmail.com>
2024-11-18 16:35:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    let skills = ['HTML', 'CSS', 'JS', 'React'];
+    let member = {
+        name: 'Jhon',
+        age: 22,
+        skills: skills
+    }
+    return member;
+}
